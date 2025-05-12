18d4798a44bb87c75e717a883c50746faafa30eb
Abimathic <abimathic14@gmail.com>
2025-05-12 12:39:43 +0100

    Move the logic for displaying the user image to a helper

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,2 +1,12 @@
 module MessagesHelper
+	def user_avatar(user, size: 40)
+	  if user.image.attached?
+	    image_tag user.image, size: "#{size}x#{size}", class: "rounded-circle me-3"
+	  else
+	    content_tag :div, user.name[0].upcase,
+	      class: "rounded-circle bg-secondary text-white d-flex justify-content-center align-items-center me-3",
+	      style: "width: #{size}px; height: #{size}px; font-size: 0.9rem;"
+	  end
+	end
+
 end
