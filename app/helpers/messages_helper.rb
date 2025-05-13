3583aba4f455204e6778a39ad627395a868185a9
Abimathic <abimathic14@gmail.com>
2025-05-13 18:27:55 +0100

    Display message date in chat view

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -9,4 +9,19 @@ module MessagesHelper
 	  end
 	end
 
+	def display_message_date(message_date, previous_date)
+		return nil if message_date == previous_date
+
+	    label =
+	      if message_date == Date.today
+	        "Today"
+	      elsif message_date == Date.yesterday
+	        "Yesterday"
+	      else
+	        message_date.strftime('%B %d, %Y') # Example: May 13, 2025
+	      end
+
+	    content_tag(:div, label, class: "text-center text-muted my-2 fw-bold")
+	end
+
 end
